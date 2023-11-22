1e12fe77571eeace2cece765742a8050f4718863
davidudo2005 <dokeregwu@gmail.com>
2023-11-22 14:00:03 +0100

    code alpha to do list

--- CodeAlpha To-Do List/script.js
+++ CodeAlpha To-Do List/script.js
@@ -1,11 +1,23 @@
 function displayText(){
     let inputText= document.getElementById("input").value;
+    console.log(inputText)
 
     let outputDiv= document.getElementById("taskbox");
 
-    let addedTask= document.createElement("p");
+    let addedTask= document.createElement("addedtask");
+    addedTask.classList.add("taskadded");
 
     addedTask.textContent = inputText;
+
+    let deleteButton=document.createElement("deletebutton")
+    deleteButton.textContent="X";
+    deleteButton.classList.add("deletetask")
+
+    deleteButton.addEventListener("click", function() {
+        outputDiv.removeChild(addedTask);
+      });
+ 
+    addedTask.appendChild(deleteButton)  
 
     outputDiv.appendChild(addedTask);
 }
